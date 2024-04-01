7b10de0acc314d9eb9f32f287ea93a8b09cd3bc0
Jackson Arnold <jackson.n.arnold@gmail.com>
2024-04-01 16:02:10 -0400

    Initial commit

---
+++ MVCDemo/src/main/java/DemoApp/Controller.java
@@ -0,0 +1,33 @@
+package DemoApp;
+
+import com.sun.tools.javac.Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Controller extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Controller.class.getResource("../Views/numberadder.fxml"));
+		
+		loader.load();
+		
+		//Scene scene = new Scene(view);
+		//primaryStage.setScene(scene);
+		//primaryStage.show();
+		
+	}
+
+}
---
+++ MVCDemo/src/main/java/DemoApp/ControllerMain.java
@@ -0,0 +1,9 @@
+package DemoApp;
+import DemoApp.Controller;
+
+public class ControllerMain {
+
+	public static void main(String[] args) {
+		Controller.main(args);
+	}
+}
